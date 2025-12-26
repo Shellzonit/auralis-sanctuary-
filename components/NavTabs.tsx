96b007ea7f60ad3223e959369402d4bb71c70c4e
Shellzonit <jons90011@gmail.com>
2025-12-25 23:31:32 -0600

    Fix NavTabs component: wrap logic in function component to resolve build error

--- components/NavTabs.tsx
+++ components/NavTabs.tsx
@@ -2,6 +2,7 @@
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 
+export default function NavTabs() {
   const pathname = usePathname();
   const tabs = [
     { href: "/", label: "Home", color: "text-fuchsia-300" },
