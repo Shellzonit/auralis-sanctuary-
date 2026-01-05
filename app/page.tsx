492ad6cf00a4717904685fbc3657d98dc1ef960a
Shellzonit <jons90011@gmail.com>
2026-01-04 20:46:37 -0600

    Remove Mr. Nanny video from homepage

--- app/page.tsx
+++ app/page.tsx
@@ -39,13 +39,6 @@ export default function HomePage() {
       <div style={{ marginTop: 48, marginBottom: 8 }}>
         <Image src="/icon.png" alt="Homepage Icon" width={120} height={120} priority style={{ borderRadius: 24, background: '#18191a' }} />
         </div>
-        {/* Mr. Nanny Video */}
-        <div style={{ marginBottom: 24 }}>
-          <video width="360" height="240" controls style={{ borderRadius: 16, boxShadow: '0 2px 16px #6a1b9a44', background: '#18191a' }}>
-            <source src="/Mr.nanny%20video.mp4" type="video/mp4" />
-            Your browser does not support the video tag.
-          </video>
-          </div>
       <h1
         style={{
           fontFamily: 'Playfair Display, Georgia, serif',
